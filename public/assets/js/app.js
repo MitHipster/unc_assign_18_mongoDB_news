949bd59bd38be2244481a6c527bfd502ccf089ee
Tim Acker <tjacker31@gmail.com>
2017-10-01 23:12:06 -0400

    Materialize
    Added materialize framework and created basic navbar partial.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,8 @@
 /*jslint esversion: 6, browser: true*/
+$(document).ready(function () {
+  // Initialize side nav bar for mobile and set width
+  $(".button-collapse").sideNav({
+    menuWidth: 250,
+    closeOnClick: true
+  });
+});
